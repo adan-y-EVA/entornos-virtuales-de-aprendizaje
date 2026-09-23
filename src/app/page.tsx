0e9458da85e0a771df9b247f229edba6edb4f10b
AnahiSU <anahi7776@gmail.com>
2026-09-23 10:24:03 -0400

    feat: integrate DashboardView in Home component and add new pages for Student Dashboard, Login, and Register

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import DashboardView from '../app/pages/Dashboard-Admin';
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-primary font-sans">
-      hello
+      <DashboardView></DashboardView>
     </div>
   );
 }
